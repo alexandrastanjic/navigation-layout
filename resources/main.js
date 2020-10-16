ab1784912dbc2c66b05d53b2e2da96d4f7c412d2
Alexandra S <alexandrastanjic@gmail.com>
2020-10-16 23:07:01 +0300

    finished the layout/started with js

---
+++ resources/main.js
@@ -0,0 +1,51 @@
+document.addEventListener("DOMContentLoaded", function (event) {
+  const menuItems = document.getElementsByClassName("menu-item");
+
+  // or forEach
+  for (let i = 0; i < menuItems.length; i++) {
+    const menuItem = menuItems[i];
+
+    menuItem.addEventListener('click', function () {
+      for (let j = 0; j < menuItems.length; j++) {
+        if (menuItems[j].classList.contains('selected')) {
+          menuItems[j].classList.remove("selected");
+        } else {
+          continue;
+        }
+      }
+      menuItem.classList.add("selected");
+
+      /////////////////////////////////////
+
+      const dataContent = menuItem.dataset.content;
+      const contentElements = document.getElementsByClassName("content");
+      for (let k = 0; k < contentElements.length; k++) {
+        if (contentElements[k].classList.contains('hidden')) {
+          continue;
+        } else {
+          contentElements[k].classList.add("hidden");
+        }
+      }
+      document.getElementsByClassName(dataContent)[0].classList.remove("hidden");
+    });
+
+
+  }
+
+
+  //////////////////////////////////////
+  const orderDetails = document.getElementById("orderDetails");
+  const orderDetailsContainer = document.getElementsByClassName("order-details-container");
+
+  // console.log(orderDetailsContainer);
+
+  orderDetails.addEventListener('click', function () {
+    orderDetailsContainer[0].classList.remove('hidden');
+    orderDetailsContainer[0].setAttribute('style', 'z-index: 6;');
+  });
+
+  // ///////////////////////////////////
+
+
+});
+
