d284a050f1cc1626068ace3b4b20e4832a9afba5
Alexandra S <alexandrastanjic@gmail.com>
2020-10-19 22:19:02 +0300

    finished the layout and javascript

--- resources/main.js
+++ resources/main.js
@@ -1,44 +1,55 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-  const topMenu = document.getElementsByClassName('top-menu-wrapper');
-  const orderDetails = document.getElementById("orderDetails");
-  const orderDetailsContainer = document.getElementsByClassName("order-details-container");
-  const menuItems = document.getElementsByClassName("menu-item");
-  
-  for (let i = 0; i < menuItems.length; i++) {
-    const menuItem = menuItems[i];
+document.addEventListener('DOMContentLoaded', function(event) {
+	const topMenu = document.getElementsByClassName('top-menu-wrapper');
+	const orderDetails = document.getElementById('orderDetails');
+	const orderDetailsContainer = document.getElementsByClassName('order-details-container');
+	const menuItems = document.getElementsByClassName('menu-item');
+	const logoImg = document.getElementById('change-img');
 
-    menuItem.addEventListener('click', function() {
-      for (let j = 0; j < menuItems.length; j++) {
-        if (menuItems[j].classList.contains('selected')) {
-          menuItems[j].classList.remove("selected");
-        } else {
-          continue;
-        }
-      }
-      menuItem.classList.add("selected");
-      
-      if (menuItem.classList.contains('selected') && menuItem.dataset.content !== 'orders-container') {
-        topMenu[0].classList.remove('hidden');
-      } else {
-        topMenu[0].classList.add('hidden');
-      }
+	for (let i = 0; i < menuItems.length; i++) {
+		const menuItem = menuItems[i];
 
-      const dataContent = menuItem.dataset.content;
-      const contentElements = document.getElementsByClassName("content");
-      for (let k = 0; k < contentElements.length; k++) {        
-        if (contentElements[k].classList.contains('hidden')) {
-          continue;
-        } else {
-          contentElements[k].classList.add("hidden");
-        }
-      }
-      document.getElementsByClassName(dataContent)[0].classList.remove("hidden");
-    });
-  }
+		menuItem.addEventListener('click', function() {
+			for (let j = 0; j < menuItems.length; j++) {
+				if (menuItems[j].classList.contains('selected')) {
+					menuItems[j].classList.remove('selected');
+				} else {
+					continue;
+				}
+			}
+			menuItem.classList.add('selected');
 
-  orderDetails.addEventListener('click', function() {
-    orderDetailsContainer[0].classList.remove('hidden');
-    orderDetailsContainer[0].classList.add('on-top');
-    topMenu[0].classList.remove('hidden');
-  });
+			if (menuItem.classList.contains('selected') && menuItem.dataset.content !== 'orders-container') {
+				topMenu[0].classList.remove('hidden');
+			} else {
+				topMenu[0].classList.add('hidden');
+			}
+
+			if (menuItems[i].dataset.content === 'orders-container') {
+				logoImg.setAttribute('src', '../assets/logo-vip.png');
+			}	else if (menuItems[i].dataset.content === 'reward-container') {
+				logoImg.setAttribute('src', '../assets/logo-gold.png');
+			} else if (menuItems[i].dataset.content === 'account-container') {
+				logoImg.setAttribute('src', '../assets/logo-elite.png');
+			} else if (menuItems[i].dataset.content === 'payment-container') {
+				logoImg.setAttribute('src', '../assets/logo-member.png');
+			}
+
+			const dataContent = menuItem.dataset.content;
+			const contentElements = document.getElementsByClassName('content');
+			for (let k = 0; k < contentElements.length; k++) {
+				if (contentElements[k].classList.contains('hidden')) {
+					continue;
+				} else {
+					contentElements[k].classList.add('hidden');
+				}
+			}
+			document.getElementsByClassName(dataContent)[0].classList.remove('hidden');
+		});
+	}
+
+	orderDetails.addEventListener('click', function() {
+		orderDetailsContainer[0].classList.remove('hidden');
+		orderDetailsContainer[0].classList.add('on-top');
+		topMenu[0].classList.remove('hidden');
+	});
 });
