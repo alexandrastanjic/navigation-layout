2a575c2b6a527e4ddae59e3ca9ed26607c327277
Alexandra S <alexandrastanjic@gmail.com>
2020-11-03 22:06:09 +0200

    added jquery for top-navbar

--- resources/main.js
+++ resources/main.js
@@ -1,127 +1,3 @@
-// $ (function() {
-// 	const topMenu = $('.top-menu-wrapper');
-// 	const orderDetails = $ ('#orderDetails');
-// 	const orderDetailsContainer = $ ('.order-details-container');
-// 	const menuItems = $ ('.menu-item');
-// 	const logoImg = $ ('#change-img');
-
-// 	for (let i = 0; i < menuItems.length; i++) {
-// 		const menuItem = menuItems[i];
-
-// 		menuItem.addEventListener('click', function() {
-// 			for (let j = 0; j < menuItems.length; j++) {
-// 				if (menuItems[j].classList.contains('selected')) {
-// 					menuItems[j].classList.remove('selected');
-// 				} else {
-// 					continue;
-// 				}
-// 			}
-// 			menuItem.classList.add('selected');
-
-// 			// if (menuItem.classList.contains('selected') && menuItem.dataset.content !== 'orders-container') {
-// 			// 	topMenu[0].classList.remove('hidden');
-// 			// } else {
-// 			// 	topMenu[0].classList.add('hidden');
-// 			// }
-
-// 			if (menuItem.classList.contains('selected') && menuItem.dataset.content !== 'orders-container') {
-// 				topMenu[0].classList.remove('hidden');
-// 			} else {
-// 				topMenu[0].classList.add('hidden');
-// 			}
-
-// 			switch (menuItems[i].dataset.content) {
-// 				case 'orders-container': logoImg.attr('src', 'assets/logo-vip.png');
-// 				break;
-// 				case 'reward-container': logoImg.attr('src', 'assets/logo-gold.png');
-// 				break;
-// 				case 'account-container': logoImg.attr('src', 'assets/logo-elite.png');
-// 				break;
-// 				case 'payment-container': logoImg.attr('src', 'assets/logo-member.png');
-// 				break;
-// 			}
-
-// 			const dataContent = menuItem.dataset.content;
-// 			console.log(dataContent);
-// 			const contentElements = document.getElementsByClassName('content');
-// 			for (let k = 0; k < contentElements.length; k++) {
-// 				if (contentElements[k].classList.contains('hidden')) {
-// 					continue;
-// 				} else {
-// 					contentElements[k].classList.add('hidden');
-// 				}
-// 			}
-// 			document.getElementsByClassName(dataContent)[0].classList.remove('hidden');
-// 		});
-
-	
-// 	}
-//   //  orderDetails.addEventListener('click', function() {
-// 	// 	orderDetailsContainer[0].classList.remove('hidden');
-// 	// 	orderDetailsContainer[0].classList.add('on-top');
-// 	// 	topMenu[0].classList.remove('hidden');
-// 	// });
-
-// });
-
-
-
-// $(function() {
-// 	const topMenu = $('.top-menu-wrapper');
-// 	const orderDetails = $('#orderDetails');
-// 	const orderDetailsContainer = $('.order-details-container');
-// 	const menuItems = $('.menu-item');
-// 	const logoImg = $('#change-img');
-
-// 	// // odata ce ai folosit deja selectorul jQuery mai sus, nu este necesar sa il folosesti si aici, menuItems e suficient
-// 	// menuItems.click(function() {
-// 	// 	//poti sa faci putin mai simplu in functie de ce am vazut data trecuta
-// 	// 	menuItems.each(function() {
-// 	// 		if ($(this).hasClass('selected')) {
-// 	// 			$(this).removeClass('selected');
-// 	// 		}
-// 	// 	});
-// 	// 	$(this).addClass('selected');
-
-// 		//nu ai nimic cu clasa content in cod, nu sunt convinsa ca asta ai vrut sa selectezi aici
-// 		const contentElements = $('.content');
-// 		const dataContent = menuItems.data($(contentElements));
-
-// 		//asta s-ar putea sa fie de ce ai nevoie aici ca si alternativa la dataset din js
-// 		$(this).data('content');
-
-// 		// tu aici mergi pe selectia elementelor cu o clasa content, care, repet nu exista la tine in cod. iti sugerez sa pornesti de la versiunea JS, nu sa repari ca risti sa vezi aceeasi logica si sa nu iti dai seama unde e gresit
-// 		$(contentElements).each(function() {
-// 			if ($(this).hasClass('hidden')) {
-// 				return;
-// 			} else {
-// 				$(this).addClass('hidden');
-// 			}
-// 			$(dataContent).removeClass('hidden');
-// 		});
-
-// 		$(menuItems).each(function() {
-
-// 			if ($(this).hasClass('selected') && $(this).data(contentElements) !== 'orders-container') {
-// 				$(topMenu).removeClass('hidden');
-// 			} else {
-// 				$(topMenu).addClass('hidden');
-// 			}
-// 		});
-// 	});
-
-// 	// nu ai nevoie de dublu selector aici, am corectat putin mai sus, aici te las pe tine sa te ocupi
-// 	$(orderDetails).on('click', function() {
-// 		$(orderDetailsContainer).removeClass('hidden');
-// 		$(orderDetailsContainer).addClass('on-top');
-// 		$(topMenu).removeClass('hidden');
-// 	});
-// });
-
-
-
-
-
 $(function() {
 	const topMenu = $('.top-menu-wrapper');
 	const orderDetails = $('#orderDetails');
@@ -132,16 +8,41 @@ $(function() {
 
 	menuItems.click(function() {
 		const dataContent = $(this).data('content');
-		contentElements.addClass('hidden');
 
+		switch (dataContent) {
+			case 'orders-container':
+				logoImg.attr('src', 'assets/logo-vip.png');
+				break;
+			case 'reward-container':
+				logoImg.attr('src', 'assets/logo-gold.png');
+				break;
+			case 'account-container':
+				logoImg.attr('src', 'assets/logo-elite.png');
+				break;
+			case 'payment-container':
+				logoImg.attr('src', 'assets/logo-member.png');
+				break;
+		}
+
+		contentElements.addClass('hidden');
 		contentElements.each(function(index, item) {
-			if($(item).hasClass(dataContent)) {
+			if ($(item).hasClass(dataContent)) {
 				$(item).removeClass('hidden');
-			} 
+			}
 		});
-	
+
 		menuItems.removeClass('selected');
 		$(this).addClass('selected');
+
+
+		// aici functioneaza doar o singura data la click iar dupa aceea nu mai apare bara de navigare - ce imi scapa?	
+		// initial am facut cu each(), dar am avut acelasi rezultat
+	
+			if ((menuItems).hasClass('selected') && dataContent === 'orders-container') {
+				topMenu.fadeOut();
+			}
+			topMenu.removeClass('hidden');
+	
 	});
 
 	orderDetails.click(function() {
@@ -149,5 +50,3 @@ $(function() {
 		topMenu.removeClass('hidden');
 	});
 });
-
-
