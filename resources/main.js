5e66ff125127c72ec21ce815b94e1740dcc5b731
Roxana Popescu <roxanateodorapopescu@yahoo.com>
2020-11-04 08:38:40 +0200

    code review

--- resources/main.js
+++ resources/main.js
@@ -9,6 +9,7 @@ $(function() {
 	menuItems.click(function() {
 		const dataContent = $(this).data('content');
 
+		// de ce crezi ca trebuia mai mult aici?
 		switch (dataContent) {
 			case 'orders-container':
 				logoImg.attr('src', 'assets/logo-vip.png');
@@ -37,12 +38,19 @@ $(function() {
 
 		// aici functioneaza doar o singura data la click iar dupa aceea nu mai apare bara de navigare - ce imi scapa?	
 		// initial am facut cu each(), dar am avut acelasi rezultat
-	
-			if ((menuItems).hasClass('selected') && dataContent === 'orders-container') {
-				topMenu.fadeOut();
-			}
+
+		//nu mai apare pentru ca nu il mai arati, tu doar il ascunzi, trebuie sa intelegi ca tu controlezi ce se intampla aici, nu codul
+		//tehnicile astea adddClass/removeClass fadeIn/fadeOut e ok sa fie folosite la packet pentru ca actioneaza diferit asupra CSS-ului, daca folosesti una dintr-o parte si una din cealalta risti sa nu fie efectul dorit
+		//fadeIn iti strica putin stilui, trebuie sa folosesti un wrapper extern pe care sa faci doar asta fadeIn, sa nu se bage in stilul tau
+
+		if ((menuItems).hasClass('selected') && dataContent === 'orders-container') {
+			//topMenu.fadeOut();
+			topMenu.addClass('hidden');
+		} else {
+			//topMenu.fadeIn();
 			topMenu.removeClass('hidden');
-	
+		}
+		
 	});
 
 	orderDetails.click(function() {
